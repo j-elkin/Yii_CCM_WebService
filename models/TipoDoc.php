99e0be28d9efac2a0519e08818a8f2d8d66c094c
Elkin Rendón <j.erendonr@gmail.com>
2015-06-17 16:34:00 -0500

    Eliminación de gitignore.   Agregación de modelo -tipo_doc-

---
+++ models/TipoDoc.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "tipo_doc".
+ *
+ * @property integer $idtipo_doc
+ * @property string $tipo_documento
+ *
+ * @property Persona[] $personas
+ */
+class TipoDoc extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'tipo_doc';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['idtipo_doc', 'tipo_documento'], 'required'],
+            [['idtipo_doc'], 'integer'],
+            [['tipo_documento'], 'string', 'max' => 45]
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'idtipo_doc' => 'Idtipo Doc',
+            'tipo_documento' => 'Tipo Documento',
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getPersonas()
+    {
+        return $this->hasMany(Persona::className(), ['tipo_doc_idtipo_doc' => 'idtipo_doc']);
+    }
+}
